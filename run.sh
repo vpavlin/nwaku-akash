61cb5386f38b2b8ce33d5c2557c2dacd6be3c169
Václav Pavlín <vaclav.pavlin@gmail.com>
2023-08-10 15:48:48 +0200

    add persistent storage, bump resources

--- run.sh
+++ run.sh
@@ -7,20 +7,23 @@ if [ -z "${DOMAIN}" ]; then
     exit 1
 fi
 
-certbot certonly\
-    --non-interactive\
-    --agree-tos\
-    --no-eff-email\
-    --no-redirect\
-    --email user@${DOMAIN}\
-    -d ${DOMAIN}\
-    --standalone
-
 LETSENCRYPT_PATH=/etc/letsencrypt/live/${DOMAIN}
 
-if ! [ -e "${LETSENCRYPTPATH}/privkey.pem" ]; then
+if ! [ -d "${LETSENCRYPT_PATH}" ]; then
+    certbot certonly\
+        --non-interactive\
+        --agree-tos\
+        --no-eff-email\
+        --no-redirect\
+        --email user@${DOMAIN}\
+        -d ${DOMAIN}\
+        --standalone
+fi
+
+if ! [ -e "${LETSENCRYPT_PATH}/privkey.pem" ]; then
     echo "The certificate does not exist"
     sleep 10
     exit 1
+fi
 
-exec "/usr/bin/wakunode" $@ --websocket-secure-key-path=${LETSENCRYPTPATH}/privkey.pem --websocket-secure-cert-path=${LETSENCRYPTPATH}/cert.pem
+exec "/usr/bin/wakunode $@ --websocket-secure-key-path=${LETSENCRYPT_PATH}/privkey.pem --websocket-secure-cert-path=${LETSENCRYPT_PATH}/cert.pem"
