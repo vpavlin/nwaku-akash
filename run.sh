de5b5d9dc39b53e4d794fc06ffcdbdf8b81bee8d
Václav Pavlín <vaclav.pavlin@gmail.com>
2023-08-10 22:02:21 +0200

    move domain name to run script

--- run.sh
+++ run.sh
@@ -26,4 +26,4 @@ if ! [ -e "${LETSENCRYPT_PATH}/privkey.pem" ]; then
     exit 1
 fi
 
-exec /usr/bin/wakunode $@ --websocket-secure-key-path=${LETSENCRYPT_PATH}/privkey.pem --websocket-secure-cert-path=${LETSENCRYPT_PATH}/cert.pem
+exec "/usr/bin/wakunode" $@ "--websocket-secure-key-path=${LETSENCRYPT_PATH}/privkey.pem" "--websocket-secure-cert-path=${LETSENCRYPT_PATH}/cert.pem" "--dns4-domain-name=${DOMAIN}"
