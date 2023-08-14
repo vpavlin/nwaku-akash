e6cfb6e1db3218b2e66a5facfeab866e45453879
Václav Pavlín <vaclav.pavlin@gmail.com>
2023-08-14 11:47:40 +0200

    add PG to deployment, tweak DF

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ set -xe
 
 if [ -z "${DOMAIN}" ]; then
     echo "You must provide a domain"
+    sleep 120
     exit 1
 fi
 
@@ -22,8 +23,12 @@ fi
 
 if ! [ -e "${LETSENCRYPT_PATH}/privkey.pem" ]; then
     echo "The certificate does not exist"
-    sleep 10
+    sleep 120
     exit 1
 fi
 
-exec "/usr/bin/wakunode" $@ "--websocket-secure-key-path=${LETSENCRYPT_PATH}/privkey.pem" "--websocket-secure-cert-path=${LETSENCRYPT_PATH}/cert.pem" "--dns4-domain-name=${DOMAIN}"
+exec "/usr/bin/wakunode" $@ \
+    "--websocket-secure-key-path=${LETSENCRYPT_PATH}/privkey.pem"\
+    "--websocket-secure-cert-path=${LETSENCRYPT_PATH}/cert.pem"\
+    "--dns4-domain-name=${DOMAIN}"\
+    "--store-message-db-url="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/waku"
